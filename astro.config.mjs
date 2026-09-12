ff5b3a67a95ba47f9d8fd5832dd1f041d127b3f5
Johnny Wu <johwuy@gmail.com>
2026-09-12 01:32:33 -0700

    Add mermaid graph support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,33 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import mermaid from "astro-mermaid";
 
 export default defineConfig({
   site: "https://johnnywu.net",
   compressHTML: true,
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [
+    mermaid({
+      theme: "neutral",
+      autoTheme: true,
+      enableLog: false,
+      mermaidConfig: {
+        fontFamily: "Instrument Sans, sans-serif",
+        flowchart: {
+          curve: "basis",
+          nodeSpacing: 36,
+          rankSpacing: 48,
+        },
+      },
+    }),
+    mdx(),
+    sitemap(),
+    tailwind(),
+  ],
+  markdown: {
+    syntaxHighlight: {
+      type: "shiki",
+      excludeLangs: ["mermaid", "math"],
+    },
+  },
 });
